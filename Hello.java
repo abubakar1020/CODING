575aa05a77cb7ea96895d3e49e0595c5a6764946
abubakar1020 <azubair1020@gmail.com>
2022-09-17 14:02:51 +0100

    make addition to Hello.java

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello{
+	public static void main (String [] args){
+		System.out.println ("Assalamu Alaikum");
+		System.out.println ("I am curious to start learning Programming");
+		System.out.println ("Thank You");
+	}
+}
