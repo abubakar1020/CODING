db70bee81039eedad554181c01f8d9f118ac1b30
abubakar1020 <azubair1020@gmail.com>
2022-09-19 19:48:37 +0100

    Greeting.java

---
+++ Greeting.java
@@ -0,0 +1,9 @@
+public class Greeting{
+	public static void main (String [] args){
+		System.out.println ("Assalamu Alaikum");
+		System.out.println ("I am curious to start learning Programming");
+		System.out.println ("Thank You");
+		System.out.println ("BYE");
+	}
+
+}
